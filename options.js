7d6f6bab6906f7073df915eae2429d66bef99495
Dinir Nertan <DinirNertan@gmail.com>
2019-06-20 01:40:27 +0900

    follow the standard style

--- options.js
+++ options.js
@@ -1,26 +1,26 @@
 var gpConstants = {
-	// how big the thumb sticks should be? (0~100)
-	thumbSize: 60
-	// what should be the color of buttons?
-	,defaultColor: "white"
-	// what color the text of the pressed button should turn to?
-	,pressedButtonColor: "orange"
-	// change the hue of buttons.
-	// this affects background color of pressed buttons as well as
-	// the default color of buttons.
-	// if you want to separately change the background color,
-	// modify the single pixel image file `dot.png`.
-	,pressedButtonBackgroundColorHue: "0"
-	// how far should the stick have moved
-	// to be considered as an intentional movement? (0~1)
-	// 0 is the center, 1 is the border.
-	,axisDeadzone: 0.05
-	// should unpressed buttons fade out? (true/false)
-	,useFadeOut: true
-	// set the seconds the fade-out starts in the array.
-	,fadeOutTime: [8, 16, 32]
-	// set the strength of the fade-out for each timing above. (0~1)
-	,fadeOutStrength: [.5, .9, 1]
-	// set the seconds each fade-out applies through.
-	,fadeOutDuration: 4
+  // how big the thumb sticks should be? (0~100)
+  thumbSize: 60,
+  // what should be the color of buttons?
+  defaultColor: "white",
+  // what color the text of the pressed button should turn to?
+  pressedButtonColor: "orange",
+  // change the hue of buttons.
+  // this affects background color of pressed buttons as well as
+  // the default color of buttons.
+  // if you want to separately change the background color,
+  // modify the single pixel image file `dot.png`.
+  pressedButtonBackgroundColorHue: "0",
+  // how far should the stick have moved
+  // to be considered as an intentional movement? (0~1)
+  // 0 is the center, 1 is the border.
+  axisDeadzone: 0.05,
+  // should unpressed buttons fade out? (true/false)
+  useFadeOut: true,
+  // set the seconds the fade-out starts in the array.
+  fadeOutTime: [8, 16, 32],
+  // set the strength of the fade-out for each timing above. (0~1)
+  fadeOutStrength: [.5, .9, 1],
+  // set the seconds each fade-out applies through.
+  fadeOutDuration: 4
 };

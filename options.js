3f122997c87cad3f109a6e3eb8b02d7a877040a4
Dinir Nertan <DinirNertan@gmail.com>
2019-06-19 20:53:56 +0900

    follow the standard style

--- options.js
+++ options.js
@@ -1,27 +1,27 @@
 var gpConstants = {
-	// how big the thumb sticks should be? (0~100)
-	thumbSize: 60
-	// what should be the color of buttons?
-	,defaultColor: "white"
-	// what color the text of the pressed button should turn to?
-	,pressedButtonColor: "orange"
-	// change the hue of buttons.
-	// this affects background color of pressed buttons as well as
-	// the default color of buttons.
-	// if you want to separately change the background color,
-	// modify the single pixel image file `dot.png`.
-	,pressedButtonBackgroundColorHue: "0"
-	// how responsive should thumb stick tracking be? (0~)
-	// it refers to this value when deciding if you moved the stick.
-	// eg) 3 -> will track the position of the stick DOM
-	// to 10^-3 below the decimal point.
-	,axisSensitivity: 0
-	// should unpressed buttons fade out? (true/false)
-	,useFadeOut: true
-	// set the seconds the fade-out starts in the array.
-	,fadeOutTime: [8, 16, 32]
-	// set the strength of the fade-out for each timing above. (0~1)
-	,fadeOutStrength: [.5, .9, 1]
-	// set the seconds each fade-out applies through.
-	,fadeOutDuration: 4
-};
+  // how big the thumb sticks should be? (0~100)
+  thumbSize: 60,
+  // what should be the color of buttons?
+  defaultColor: '#65849f',
+  // what color the text of the pressed button should turn to?
+  pressedButtonColor: 'orange',
+  // change the hue of buttons.
+  // this affects background color of pressed buttons as well as
+  // the default color of buttons.
+  // if you want to separately change the background color,
+  // modify the single pixel image file `dot.png`.
+  pressedButtonBackgroundColorHue: '0',
+  // how responsive should thumb stick tracking be? (0~)
+  // it refers to this value when deciding if you moved the stick.
+  // eg) 3 -> will track the position of the stick DOM
+  // to 10^-3 below the decimal point.
+  axisSensitivity: 0,
+  // should unpressed buttons fade out? (true/false)
+  useFadeOut: true,
+  // set the seconds the fade-out starts in the array.
+  fadeOutTime: [8, 16, 32],
+  // set the strength of the fade-out for each timing above. (0~1)
+  fadeOutStrength: [0.5, 0.9, 1],
+  // set the seconds each fade-out applies through.
+  fadeOutDuration: 4
+}

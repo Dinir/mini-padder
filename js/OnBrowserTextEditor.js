3d1769e9a4ead18bfd805ec12a84fa38c64ea05e
Dinir Nertan <DinirNertan@gmail.com>
2019-09-26 14:42:35 +0900

    clean up

--- js/OnBrowserTextEditor.js
+++ js/OnBrowserTextEditor.js
@@ -3,9 +3,8 @@
  * @property {callback} save function to call when user tries to save
  * @property {callback} load function to clal when user tries to load
  */
-
 /**
- * @typedef {Object} TextEditorDOMs
+ * @typedef {Object} TextEditorDOM
  * @property {HTMLElement} wrapper a div wrapping all the elements
  * @property {HTMLTextAreaElement} textarea
  * @property {HTMLButtonElement} saveButton
@@ -13,14 +12,11 @@
  */
 
 /**
- * Create a textarea and a set of buttons to interact with a text data.
+ * Create a textarea and a set of buttons to give access to the text
  * @class
  */
 class OnBrowserTextEditor {
-  /**
-   *
-   * @param {TextEditorCallbacks} callbacks
-   */
+  /** @param {TextEditorCallbacks} callbacks */
   constructor (callbacks) {
     this.textarea = document.createElement('textarea')
   
@@ -32,8 +28,8 @@ class OnBrowserTextEditor {
      *   window.localStorage.setItem('text', text)
      * }
      *
-     * @param event the event that comes from `onclick`
-     * @param text text written in the textarea
+     * @param {MouseEvent} event the event that comes from `onclick`
+     * @param {String} text text written in the textarea
      */
     this.save = (event, text = this.textarea.value) =>
       callbacks.save(text)
@@ -45,32 +41,35 @@ class OnBrowserTextEditor {
      *   textarea.value = window.localStorage.getItem('text')
      * }
      *
-     * @param event the event that comes from `onclick`
-     * @param textarea
-     * @returns {*}
+     * @param {MouseEvent} event the event that comes from `onclick`
+     * @param {HTMLTextAreaElement} textarea to populate
      */
     this.load = (event, textarea = this.textarea) =>
       callbacks.load(textarea)
     
     this.saveButton = document.createElement('button')
+    this.saveButton.setAttribute('class', 'save-button')
     this.saveButton.onclick = this.save.bind(this)
-    this.saveButton.textContent = 'Save to Page'
+    this.saveButton.textContent =
+      'Save Current Text as Configuration'
+    
     this.loadButton = document.createElement('button')
+    this.loadButton.setAttribute('class', 'load-button')
     this.loadButton.onclick = this.load.bind(this)
-    this.loadButton.textContent = 'Load Current Configuration'
+    this.loadButton.textContent =
+      'Load Current Configuration'
   
     this.wrapper = document.createElement('div')
-    this.wrapper.id = 'text-editor-wrapper'
+    this.wrapper.setAttribute('id', 'text-editor-wrapper')
     this.wrapper.appendChild(this.textarea)
     this.wrapper.appendChild(this.saveButton)
     this.wrapper.appendChild(this.loadButton)
-    
   }
   
   /**
-   * @returns {TextEditorDOMs} the page then can use these elements
+   * @returns {TextEditorDOM} the page then can use these elements
    */
-  getDOMs () {
+  getDOM () {
     return {
       wrapper: this.wrapper,
       textarea: this.textarea,

950e07ac2522fcdda76a0c8e550f0a6b5c4ac2ae
Dinir Nertan <DinirNertan@gmail.com>
2019-11-03 21:08:08 +0900

    add background circles for axes
    when playing a game that destroys the encoding, it's easier to tell the axis position when you can see the entire area for the axis.

--- options.js
+++ options.js
@@ -5,6 +5,8 @@ var gpConstants = {
   defaultFont: 'Fira Code',
   // what should be the color of buttons?
   defaultColor: '#65849f',
+  // what should be the background color for axes?
+  axesBgColor: 'rgba(0, 0, 0, 0.8)',
   // what color the text of the pressed button should turn to?
   pressedButtonColor: 'orange',
   // change the hue of buttons.

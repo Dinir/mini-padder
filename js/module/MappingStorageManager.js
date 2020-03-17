978ce9af90d4ce5dddcca16090467d59ae0afde5
Dinir Nertan <DinirNertan@gmail.com>
2020-03-17 18:36:29 +0900

    rewrite the mapping format

--- js/module/MappingStorageManager.js
+++ js/module/MappingStorageManager.js
@@ -1,14 +1,48 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 /**
- * @typedef {Object} gamepadMapping
- * @property {String} identifier human readable string to tell what this mapping is for
- * @property {?Object} windows set of buttons and axes used in Windows, should exist if the mapping is used on Windows.
- * @property {{label: String, type: String}[]} windows.buttons
- * @property {{label: String, type: String}[]} windows.axes
- * @Property {?Object} linux set of buttons and axes used in Linux, should exist if the mapping is used on Linux.
- * @property {{label: String, type: String}[]} linux.buttons
- * @property {{label: String, type: String}[]} linux.axes
+ * @typedef gamepadMapping
+ * @type {Object}
+ * @description contains which function of a gamepad can be found in which index of axis or buttons in the corresponding Gamepad object.
+ * @property {String} name human readable string to tell what this mapping is for
+ * @property {String[]} properties gamepad-specific features. All strings in this array should be defined in a render logic.
+ *
+ * @property {Object.<string, (number|Object.<string, number>)>} axes
+ *
+ * @property {number} axes.deadzone define the end of a range of value which shouldn't be treated as an actual movement. 0 is at the center, 1 is the end of the axis.
+ * @property {{x: number, y: number, button: number}} axes.left indexes of axes and a button for left thumb stick
+ * @property {{x: number, y: number, button: number}} axes.right numbers of axes and a button for right thumb stick
+ *
+ * @property {Object.<string, Object.<string, number>>} buttons
+ *
+ * @property {Object.<string, number>} buttons.dpad indexes of buttons for dpad. When a gamepad conveys the dpad state using an axis, there should be `axis` and `precision` properties, also direction properties should then point at values the axis represents when the dpad is pressed so, instead of the indexs of each buttons for dpad.
+ * @property {number} buttons.dpad.up
+ * @property {number} buttons.dpad.down
+ * @property {number} buttons.dpad.left
+ * @property {number} buttons.dpad.right
+ * @property {number} [buttons.dpad.axis] the axis dpad state is conveyed
+ * @property {number} [buttons.dpad.precision] range used to define if current axis value is 'close enough' to a value defined in direction properties
+ * @property {number} [buttons.dpad.upright]
+ * @property {number} [buttons.dpad.downright]
+ * @property {number} [buttons.dpad.downleft]
+ * @property {number} [buttons.dpad.upleft]
+ *
+ * @property {Object.<string, number>} buttons.face
+ * @property {number} buttons.face.down index of down face button. Typically A in XInput controllers.
+ * @property {number} buttons.face.right index of right face button. Typically B in XInput controllers.
+ * @property {number} buttons.face.left index of left face button. Typically X in XInput controllers.
+ * @property {number} buttons.face.up index of up face button. Typically Y in XInput controllers.
+ * @property {number} buttons.face.select index of select button
+ * @property {number} buttons.face.start index of start button
+ * @property {number} [buttons.face.home] index of home button. Typically a logo button in gamepads.
+ * @property {number} [buttons.face.touchpad] index of the touchpad button on DualShock4 controller. It can only track the click the touchpad makes.
+ *
+ * @property {Object.<string, number>} buttons.shoulder
+ * @property {number} buttons.shoulder.l1
+ * @property {number} buttons.shoulder.r1
+ * @property {number} [buttons.shoulder.l2]
+ * @property {number} [buttons.shoulder.r2]
+ *
  */
 /**
  * @typedef {string} gamepadId 8-digit hexadecimal string

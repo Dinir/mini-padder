72a2c06287e693429c99a3b685054d35f399ba45
Dinir Nertan <DinirNertan@gmail.com>
2020-09-11 20:56:08 +0900

    make button value off check flexible
    It will always check the value's two members, then the value itself.
    If the value is a number (most of the cases), it will first always be 'off' then second phrase will properly check if it's off, and correct the final result.
    If the value is an array of two numbers (dpad value), it will be properly checked by the first phrase, skipping the second phrase.
    
    This change should make the check always pass the proper result regardless of the button value's type, making it possible to make a skin that uses dpad value and left analog stick at the same time.

--- js/module/GamepadRenderer.js
+++ js/module/GamepadRenderer.js
@@ -1224,9 +1224,10 @@ class GamepadRenderer {
                 null, null, null
               )
           
-              // joystick skin uses dpad.value which is [x-axis, y-axis]
-              const valueIsOff = forJoystick && bg === 0 && b === 0 ?
-                value[0] === 0 && value[1] === 0 : value === 0
+              // dpad.value is [x-axis, y-axis],
+              // other button values are number
+              const valueIsOff =
+                value[0] === 0 && value[1] === 0 || value === 0
               if (valueIsOff) {
                 this.followInstructions(
                   ctx[buttonLayerIndex], src, buttonInst.off,

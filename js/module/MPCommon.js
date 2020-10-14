ac830415768d9ad25f833a6de9dc63dc87c03b1d
Dinir Nertan <DinirNertan@gmail.com>
2020-10-14 16:59:05 +0900

    add method to check possible frame losses

--- js/module/MPCommon.js
+++ js/module/MPCommon.js
@@ -1,5 +1,21 @@
 class MPCommon {
   /**
+   * the desired fps to run the app under
+   * @type {number}
+   */
+  static fps = 60
+  /**
+   * the length one frame covers in milliseconds
+   * @type {number}
+   */
+  static frameLength = 1000 / MPCommon.fps
+  /**
+   * the max difference in length allowed for one frame in milliseconds squared
+   * @type {number}
+   */
+  static maxSquareDiffAllowedInFrame = 16 ** 2
+  
+  /**
    * the last confirmed maximum number of gamepads available
    * @type {number}
    */
@@ -25,6 +41,50 @@ class MPCommon {
         }
       }))
     }
+  }
+  
+  /**
+   * Calculate the difference between two timestamps and return it.
+   * Or if it's the first frame, return `true`.
+   *
+   * At ignition of a callback that should be looped on animated frames,
+   * the timestamp may not be given to it and is seen as `undefined`.
+   * I will consider this as a case where the calculation is not required,
+   * and return `true`.
+   *
+   * @param {?DOMHighResTimeStamp} currentTimestamp
+   * @param {?DOMHighResTimeStamp} pastTimestamp
+   * @returns {boolean|number}
+   * `true` at first frame, otherwise difference between two frames
+   */
+  static getFrameInterval (currentTimestamp, pastTimestamp) {
+    if (
+      currentTimestamp === undefined ||
+      pastTimestamp === null
+    ) { return true }
+    return currentTimestamp - pastTimestamp
+  }
+  
+  /**
+   * Calculate the difference between two timestamps and
+   * return true when the interval is bigger than it should be.
+   *
+   * @param {?DOMHighResTimeStamp} currentTimestamp
+   * @param {?DOMHighResTimeStamp} pastTimestamp
+   *
+   * @returns {boolean} `true` if the interval was bigger
+   * than the specified value in {@link MPCommon.maxSquareDiffAllowedInFrame}
+   */
+  static isIntervalBigEnough (currentTimestamp, pastTimestamp) {
+    if (
+      currentTimestamp === undefined ||
+      pastTimestamp === null
+    ) { return false }
+    const intervalDifference =
+      currentTimestamp - pastTimestamp - MPCommon.frameLength
+    const squareDiff = intervalDifference * intervalDifference
+    
+    return squareDiff > MPCommon.maxSquareDiffAllowedInFrame
   }
   
   /**

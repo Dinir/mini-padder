81e1e18fe547661512c74ccbe85f4d3eab28c0a8
Dinir Nertan <DinirNertan@gmail.com>
2020-10-14 21:47:19 +0900

    change timestamps in errorlog to local time

--- js/module/ErrorLogCollector.js
+++ js/module/ErrorLogCollector.js
@@ -19,12 +19,11 @@ class ErrorLogCollector {
   }
   
   /**
-   * Return timestamp in a specific format.
-   * @returns {string} DOMHighResTimeStamp
-   * truncated to 3 digits below decimal point
+   * Return time string.
+   * @returns {string}
    */
-   static get #timestamp () {
-    return (Math.floor(1000*performance.now())/1000).toFixed(3)
+   static get #time () {
+     return new Date().toLocaleTimeString()
    }
   
   /**
@@ -37,7 +36,7 @@ class ErrorLogCollector {
    * @returns {boolean}
    */
   write (message, url, lineNo, columnNo, error) {
-    this.errorLog[0] += `[${ErrorLogCollector.#timestamp}] `
+    this.errorLog[0] += `[${ErrorLogCollector.#time}] `
     this.errorLog[0] += error instanceof Error ? error.stack : message
     this.errorLog[0] += '\n'
     return false
